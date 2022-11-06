0c2d9a90e7036956514b0e4d99554ddaabb0df2c
L0rd1k <ilua107@gmail.com>
2022-11-06 20:08:06 +0300

    Add last updates

--- cmd/lightvss/main.go
+++ cmd/lightvss/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func test_handler(_writer http.ResponseWriter, _request *http.Request) {
+	fmt.Fprintf(_writer, "Jopa %s", _request.URL.Path)
+}
+
+func test_handler_2(_writer http.ResponseWriter, _request *http.Request) {
+	for k, v := range _request.Header {
+		fmt.Fprintf(_writer, "Header[%q] = %q\n", k, v)
+	}
+}
+
 func main() {
 
-	_myhandler := func(_writer http.ResponseWriter, _request *http.Request) {
-		io.WriteString(_writer, "Check state!\n")
-	}
+	// _myhandler := func(_writer http.ResponseWriter, _request *http.Request) {
+	// 	io.WriteString(_writer, "Check state!\n")
+	// }
+	// http.HandleFunc("/hello", _myhandler)
+	// log.Println("Listen for request!")
 
-	http.HandleFunc("/hello", _myhandler)
-	log.Println("Listen for request!")
+	http.HandleFunc("/", test_handler)
+	http.HandleFunc("/hello", test_handler_2)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
